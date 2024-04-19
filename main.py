196d68e38888febedbdda1d14a5b51615e439e00
Olya Milch <323234214213m@gmail.com>
2024-04-19 19:22:05 +0300

    Add main file

---
+++ main.py
@@ -0,0 +1,21 @@
+import os  # библиотека, позволяет работать с системой и файлам
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+dirname = os.path.dirname(__file__)  # ищем  путь
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'  # путь к базе данных
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # отключаем предупреждения об изменения от Фласк
+db = SQLAlchemy(app)
+
+
+@app.route("/")
+def my_lover_function():
+    return 'дайте хоть шо-то'
+
+
+from ingredients import *
+
+if __name__ == "__main__":
+    app.run(debug=True)  # пока тру, т.к. надо видеть ошибки. Потом сменить на фолс, чтобы клиенты их не видели
