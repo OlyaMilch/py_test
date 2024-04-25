9d00bd95450ecefb8b4eb155ceece0fc60937803
Olya Milch <323234214213m@gmail.com>
2024-04-25 21:22:36 +0300

    Add method 'Delete', changed method 'Get' and fixed Primary Key

--- ingredients.py
+++ ingredients.py
@@ -1,35 +1,35 @@
-from flask import Flask, jsonify, request
-from flask_sqlalchemy import SQLAlchemy  # конструктор приложений
-
-from main import db
+from flask import jsonify, request, Flask
+from flask_sqlalchemy import SQLAlchemy
 from main import app
+from main import db
+
 
 # Определяем модель базы данных (в каком столбе какой тип данных)
 
 class Ingredient(db.Model):
-    id = db.Column(db.Integer, primary_key=False)
+    id_i = db.Column(db.Integer, unique=True, primary_key=True)
     title = db.Column(db.String(80), unique=False, nullable=False)
     category = db.Column(db.String(120), unique=False, nullable=False)
 
     # Упрощаем читаемость кода
     def __repr__(self):
-        return '<Ingredient %r>' % self.title
+        return f'<Ingredient {self.title}'
 
 
 #  Получаем информацию по ингредиенту
 @app.route('/ingredients', methods=['GET'])  # Смотрим весь список
-def get_users():
+def get_ingredient():
     ingredients = Ingredient.query.all()
     return jsonify(
-        [{'id': Ingredient.id, 'username': Ingredient.title, 'category': Ingredient.category} for Ingr in
+        [{'id_i': Ingredient.id_i, 'username': Ingredient.title, 'category': Ingredient.category} for Ingr in
          ingredients])
 
 
 # Получаем информацию по айди
-@app.route('/id', methods=['GET'])
-def get_id_ingredients():
-    db.create_all()
-    return ""
+@app.route('/id_i/<int:id_i>', methods=['GET'])
+def get_id_ingredient(id_i):
+    ingredient = Ingredient.query.get(id_i)
+    return jsonify(ingredient)
 
 
 # Добавляем новую запись (требуется название и категория ингредиента, айди само поставится по порядку)
@@ -39,4 +39,16 @@ def create_ingredient():
     new_ingredient = Ingredient(title=data['title'], category=data['category'])
     db.session.add(new_ingredient)
     db.session.commit()
-    return jsonify({'id': new_ingredient.id, 'title': new_ingredient.title, 'category': new_ingredient.category}), 201
+    return jsonify(
+        {'id_i': new_ingredient.id_i, 'title': new_ingredient.title, 'category': new_ingredient.category}), 201
+
+
+# удаление информации о ингредиенте
+@app.route('/delete/<int:id>', methods=['DELETE'])
+def delete_recipe(id_i: int):
+    try:
+        ingredient = Ingredient.query.get(id_i)  # query - запрос
+        db.session.delete(ingredient)  # удаление всей записи по id
+        db.session.commit()
+    except Exception:
+        return "Ингредиент не найден"
